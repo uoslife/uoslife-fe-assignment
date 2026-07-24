c585a389187eef6514d99dcdecc4f86f1bafed61
vallltru <vallltru@gmail.com>
2026-07-25 05:35:58 +0900

    chore: 미사용 테마 정리

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,6 @@
 export const theme = {
     colors: {
         white: '#FFFFFF',
-        black: '#000000',
         primary: '#3766A4',
         primaryDark: '#102743',
         background: '#E1EBFF',
